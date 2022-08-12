905784a35b2dfb855f89efed291527b82913ebf9
Clarisanchez <106829055+Clarisanchez@users.noreply.github.com>
2022-08-12 17:38:34 -0300

    Delete punto 3 Python.py

--- punto 3 Python.py
+++
@@ -1,6 +0,0 @@
-Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-Perro=[13, "Puppy", "12/12/2012", "Macho",123]
-Perro[2]="13/12/2012"
-Perro[4]="28957346"
-print(Perro)
