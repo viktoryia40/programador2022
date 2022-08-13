aba833bd595624b73faa5fdf368539a404d4aa71
viktoryia40 <ozarenko2719@gmail.com>
2022-08-13 00:17:06 -0300

    comment

---
+++ VolaracionFinal.sql
@@ -0,0 +1,4 @@
+/*9. Una consulta que permita actualizar 
+la dirección de un dueño. Su nueva dirección es Libertad 123*/
+
+update Dueno set Dirección='Libertad 123' where Nombre in ("Juan Carlos");
